a00cc35eb7969cfacb85904f371494ed5105d632
kasimoviw60-cmd <kasimoviw60@gmail.com>
2026-05-02 21:52:08 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram import html # HTML formatlash uchun qo'shildi
 
 # --- KONFIGURATSIYA ---
-TOKEN = "8251656306:AAE9fplew22iEWQPFOZbVdVzoHMFttUQaM8"
+TOKEN = "8735179134:AAFLIn8G-SzHY6r3tlQqHvL8XGj4_Q3TNs8"
 
 CHANNELS = [
     {"id": "@instagram_kasimov", "url": "https://t.me/instagram_kasimov"},
